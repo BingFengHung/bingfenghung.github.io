865a6fdebfef6e263fb03d050d52c4096a8bd97a
BingFengHung <joe94008@gmail.com>
2021-08-16 15:05:40 +0000

    確認 FAQ 頁面切換正常

--- src/views/faq/Faq.js
+++ src/views/faq/Faq.js
@@ -15,35 +15,45 @@ function Faq() {
 	const linux = useRef(null);
 	const others = useRef(null);
 
+	const csharp_tab_content = useRef(null);
+	const javascript_tab_content = useRef(null);
+	const python_tab_content = useRef(null);
+	const linux_tab_content = useRef(null);
+	const others_tab_content = useRef(null);
+
 	const csharp_content = useRef(null);
 	const javascript_content = useRef(null);
 	const python_content = useRef(null);
 	const linux_content = useRef(null);
 	const others_content = useRef(null);
 
-	function openTab(element, color) {
-		if(element === null || element.current === null) {
-			// console.log(element.current)
-			return
-		}
-		console.log(element)
-
+	function openTab(title, content, color) {
+		console.log('ins')
+		if (title === null || title.current === null) return;
 		let tab = [csharp, javascript, python, linux, others];
+		let tabContent = [csharp_tab_content, javascript_tab_content, python_tab_content, linux_tab_content, others_tab_content]
 		let articleContent = [csharp_content, javascript_content, python_content, linux_content, others_content]
 
-		console.log(csharp.current)
 		tab.forEach(element => {
-			element.current.style.display='none'
+			element.current.style.backgroundColor = ''
 		});
+
+		tabContent.forEach(element => {
+			element.current.style.display = 'none'
+		})
 
 		articleContent.forEach(element => {
 			element.current.style.display='none'
 		})
 
-		element.current.style.backgroundColor = color;
-		element.current.style.display = 'block';
+		title.current.style.backgroundColor = color;
+		title.current.style.display = 'block';
 
-		let idx = tab.indexOf(element)
+		let idx = tab.indexOf(title)
+		tabContent[idx].current.style.display='block';
+		tabContent[idx].current.style.backgroundColor = color
+		title.current.style.backgroundColor = color
+
 		articleContent[idx].current.style.display = 'flex';
 	}
 
@@ -56,80 +66,78 @@ function Faq() {
 		})
 		.then(res => res.json())
 		.then(data => {
-			let csharp = data.CSharp;
-			let javascript = data.JavaScript;
-			let python = data.Python;
-			let linux = data.Linux;
-			let others = data.Others;
+			let csharpDoc = data.CSharp;
+			let javascriptDoc = data.JavaScript;
+			let pythonDoc = data.Python;
+			let linuxDoc = data.Linux;
+			let othersDoc = data.Others;
 
 			let dataList = []
-			csharp.forEach((element, idx) => {
+			csharpDoc.forEach((element, idx) => {
 				dataList.push(<Card key={idx} title={element.title} description={element.description} img={element.img} link={element.link} />);
 			});
 			setCSharpArticleData(dataList);
 
 			dataList = []
-			javascript.forEach((element, idx) => {
+			javascriptDoc.forEach((element, idx) => {
 				dataList.push(<Card key={idx} title={element.title} description={element.description} img={element.img} link={element.link} />);
 			});
 			setJavaScriptArticleData(dataList);
 
 			dataList = []
-			python.forEach((element, idx) => {
+			pythonDoc.forEach((element, idx) => {
 				dataList.push(<Card key={idx} title={element.title} description={element.description} img={element.img} link={element.link} />);
 			});
 			setPythonArticleData(dataList);
 
 			dataList = []
-			linux.forEach((element, idx) => {
+			linuxDoc.forEach((element, idx) => {
 				dataList.push(<Card key={idx} title={element.title} description={element.description} img={element.img} link={element.link} />);
 			});
 			setLinuxArticleData(dataList);
 
 			dataList = []
-			others.forEach((element, idx) => {
+			othersDoc.forEach((element, idx) => {
 				dataList.push(<Card key={idx} title={element.title} description={element.description} img={element.img} link={element.link} />);
 			});
 			setOthersArticleData(dataList);
+
+			openTab(csharp, csharp_content,'red')
 		})
 	}, []);
 
 	return (
-		<div>
-			<div className={styles.tab_content}>
+		<div className={styles.container}>
+			<div ref={csharp_tab_content} className={styles.tab_content}>
 				<h1>C#</h1>
 				<p>C# / .NET 經驗談</p>
 			</div>
 
-			<div className={styles.tab_content}>
+			<div ref={javascript_tab_content} className={styles.tab_content}>
 				<h1>JavaScript</h1>
 				<p>前端、後端一把罩</p>
 			</div>
 
-			<div className={styles.tab_content}>
+			<div ref={python_tab_content} className={styles.tab_content}>
 				<h1>Python</h1>
 				<p>當代 AI 熱門語言首選</p>
 			</div>
 			
-			<div className={styles.tab_content}>
+			<div ref={linux_tab_content} className={styles.tab_content}>
 				<h1>Linux</h1>
 				<p>無所不在的 OS 系統</p>
 			</div>
 
-			<div className={styles.tab_content}>
+			<div ref={others_tab_content} className={styles.tab_content}>
 				<h1>Others</h1>
 				<p>其他雜談</p>
 			</div>
 
-			<button ref={csharp} onClick={openTab(csharp, 'red')}>CSharp</button> 
-
-			<button ref={javascript} onClick={openTab(javascript, 'green')}>JavaScript</button> 
-
-			<button ref={python} onClick={openTab(python, 'blue')}>Python</button> 
-
-			<button ref={linux} onClick={openTab(linux, 'orange')}>Linux</button> 
-
-			<button ref={others} onClick={openTab(others, 'yellowgreen')}>其他</button> 
+			<button ref={csharp} className={styles.tab_link} onClick={() => openTab(csharp, csharp_content, 'red')}>CSharp</button> 
+			<button ref={javascript} className={styles.tab_link} onClick={() => openTab(javascript, javascript_content, 'green')}>JavaScript</button> 
+			<button ref={python} className={styles.tab_link} onClick={() => openTab(python, python_content, 'blue')}>Python</button> 
+			<button ref={linux} className={styles.tab_link} onClick={()=>openTab(linux, linux_content, 'orange')}>Linux</button> 
+			<button ref={others} className={styles.tab_link} onClick={() => openTab(others, others_content, 'yellowgreen')}>其他</button> 
 			
 			{/* Content */}
 			<div ref={csharp_content} className={styles.content}>
