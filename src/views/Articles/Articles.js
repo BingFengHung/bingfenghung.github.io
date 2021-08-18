0743eb98350ad6cf21676dafa0c4ceb4e5801eb0
BingFengHung <joe94008@gmail.com>
2021-08-18 00:24:45 +0000

    調整 FAQ 畫面的版面樣式與 markdown 的程式碼顏色標註

--- src/views/Articles/Articles.js
+++ src/views/Articles/Articles.js
@@ -2,31 +2,36 @@ import styles from './Articles.module.css'
 import { useEffect, useState } from 'react';
 import marked from 'marked';
 import hljs  from 'highlight.js'
-import 'highlight.js/styles/github.css';
+import "./Articles.css"
+// import 'highlight.js/styles/brown-paper.css';
+// import 'highlight.js/styles/idea.css';
+import 'highlight.js/styles/googlecode.css';
+// import 'highlight.js/styles/idea.css';
 
 marked.setOptions({
   renderer: new marked.Renderer(), 
 	highlight: function(code) {
 		return hljs.highlightAuto(code).value;
   },
-	 gfm: true,
-    pedantic: false,
-    sanitize: false,
-    tables: true,
-    breaks: true,
-    smartLists: true,
-    smartypants: true,
+	 gfm: true, 
+	 pedantic: false, 
+	 sanitize: false, 
+	 tables: true, 
+	 breaks: true, 
+	 smartLists: true, 
+	 smartypants: true,
 });
 
 function Articles(props) {
-	// const mdPath = './' + props.links
 	const [markdown, setMarkdown] = useState(null);
+	const links = localStorage.getItem("links") 
+	 const mdPath = '/' + links
+	console.log(links)
 
 	useEffect(() => {
-		const links = localStorage.getItem("links")
-
-		fetch(links)
+		fetch(mdPath)
 		.then(response => {
+			console.log(response)
 			return response.text()
 		})
 		.then(text => {
